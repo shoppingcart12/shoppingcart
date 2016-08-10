eb8c2d98b2a19823fd5c348c022d69f50a98a5c8
viswanadh9490 <kasisharma9866@gmail.com>
2016-08-10 17:13:17 +0530

    sql file
    database tablescreation in postgres sql

---
+++ db.sql
@@ -0,0 +1,12 @@
+create table roles(roleid integer primary key, rolename varchar(20));
+
+create table users(username varchar(30), userid integer primary key, password varchar(10), email varchar(20), roleid integer references roles(roleid),productid integer references product(productid));
+
+create table product(productid integer primary key, productname varchar(30),price integer, brand varchar(20), imgpath varchar(500));
+
+create table prodspecification(productid integer references product(productid), model varchar(20) );
+
+
+
+
+
